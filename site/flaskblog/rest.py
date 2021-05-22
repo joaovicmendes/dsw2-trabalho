0d78b5ad43b7b82b9b808660b4213400c54116a1
heckmartin <martinheck99@outlook.com>
2021-05-21 23:26:51 -0300

    Crd de promocao

--- site/flaskblog/rest.py
+++ site/flaskblog/rest.py
@@ -52,11 +52,15 @@ def create_site():
 @app.route('/api/site/<id>', methods=['DELETE'])
 def delete_site(id):
     site = Site.query.filter_by(id=id).first()
+    promos = Promo.query.filter_by(site_end=site.endereco).all()
     if not site:    
         return jsonify({'message':'No site found. :('})
     db.session.delete(site)
     db.session.commit()
-    return jsonify({'message':"The site has been deleted"})
+    for promo in promos:
+        db.session.delete(promo)
+        db.session.commit()
+    return jsonify({'message':"The site and all of his promos have been deleted"})
 
 
 
@@ -107,9 +111,80 @@ def create_hotel():
 @app.route('/api/hotel/<id>', methods=['DELETE'])
 def delete_hotel(id):
     hotel = Hotel.query.filter_by(id=id).first()
+    promos = Promo.query.filter_by(hotel_cnpj=hotel.cnpj).all()
     if not hotel:    
         return jsonify({'message':'No hotel found. :('})
     db.session.delete(hotel)
     db.session.commit()
+    for promo in promos:
+        db.session.delete(promo)
+        db.session.commit()
     return jsonify({'message':"The hotel has been deleted"})
 
+
+
+
+@app.route('/api/promocao', methods=['GET'])
+def get_all_promos():
+    promos = Promo.query.all()
+
+    if not promos:    
+        return jsonify({'message':'No hotel found. :('})
+
+    output = []
+    for promo in promos:
+        promo_data = {}
+        promo_data['id'] = promo.id
+        promo_data['site'] = promo.site_end
+        promo_data['cnpj'] = promo.hotel_cnpj
+        promo_data['preco'] = promo.preco
+        promo_data['inicio'] = promo.ini_promo
+        promo_data['Fim'] = promo.end_promo
+        output.append(promo_data)
+    return jsonify({'promos':output})
+
+@app.route('/api/promocao/<id>', methods=['GET'])
+def get_one_promo(id):
+    promo = Promo.query.filter_by(id=id).first()
+
+    if not promo:    
+        return jsonify({'message':'No hotel found. :('})
+
+    promo_data = {}
+    promo_data['id'] = promo.id
+    promo_data['site'] = promo.site_end
+    promo_data['cnpj'] = promo.hotel_cnpj
+    promo_data['preco'] = promo.preco
+    promo_data['inicio'] = promo.ini_promo
+    promo_data['Fim'] = promo.end_promo
+    return jsonify({'promo':promo_data})
+
+
+@app.route('/api/promocao', methods=['POST'])
+def create_promo():
+    data = request.get_json()
+    site = Site.query.filter_by(endereco=data['endereco']).first()
+    if not site:
+        return jsonify({'message':'No site found. :('})
+    site_end = site.endereco
+        
+    hotel = Hotel.query.filter_by(cnpj=data['hotel_cnpj']).first()
+    if not hotel:
+        return jsonify({'message':'No hotel found. :('})
+    hotel_cnpj = hotel.cnpj
+
+    new_promo = Promo(site_end= site_end,
+                    hotel_cnpj= hotel_cnpj,
+                    preco=data['preco'])
+    db.session.add(new_promo)
+    db.session.commit()
+    return jsonify({'message' : 'New promo included.'})
+
+@app.route('/api/promocao/<id>', methods=['DELETE'])
+def delete_promo(id):
+    promo = Promo.query.filter_by(id=id).first()
+    if not promo:    
+        return jsonify({'message':'No promo found. :('})
+    db.session.delete(promo)
+    db.session.commit()
+    return jsonify({'message':"The promo has been deleted"})
